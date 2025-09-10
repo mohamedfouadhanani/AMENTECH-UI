10e40d6ce8c50461a9b95eac1d2bfd76609c620b
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2025-09-10 15:00:06 +0100

    testing automatic netlify deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className='text-3xl text-yellow-500'>Hello, World</h1>
+      <h1 className='text-3xl text-blue-500'>Hello, AMENTECH</h1>
     </>
   )
 }
