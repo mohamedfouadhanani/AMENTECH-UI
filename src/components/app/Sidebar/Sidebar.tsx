752276b12381f2e66cc2460dee3cb0093046faf6
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2025-09-10 17:20:46 +0100

    upd: opening the sidebar collapsible by default

--- src/components/app/Sidebar/Sidebar.tsx
+++ src/components/app/Sidebar/Sidebar.tsx
@@ -8,15 +8,18 @@ import { ChevronDown, Users } from "lucide-react";
 import Element from "./Element";
 
 import POKFIT from "../../../assets/POKFIT..png";
+import { useState } from "react";
 
 function AppSidebar() {
+	const [open, setOpen] = useState(true);
+
 	return (
 		<Sidebar className="!border-r-0">
 			<SidebarContent className="p-4 space-y-8 bg-white">
 				<h1 className="font-bold text-2xl flex items-center justify-center">
 					<img src={POKFIT} alt="logo" />
 				</h1>
-				<Collapsible className="space-y-2">
+				<Collapsible open={open} onOpenChange={setOpen} className="space-y-2">
 					<CollapsibleTrigger className="text-sm flex items-center justify-between space-x-2 w-full bg-blue-400 text-white p-2 rounded-md">
 						<Users className="h-6" />
 						<span className="block flex-1 text-left">Users</span>
