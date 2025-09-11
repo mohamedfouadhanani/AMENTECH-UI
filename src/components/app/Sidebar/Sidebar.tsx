d342a0ded8b48cd10a5b3c7a99296f373c3ff651
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2025-09-11 11:01:10 +0100

    upd: added the connecting lines between the sidebar links and sublinks

--- src/components/app/Sidebar/Sidebar.tsx
+++ src/components/app/Sidebar/Sidebar.tsx
@@ -25,11 +25,29 @@ function AppSidebar() {
 						<span className="block flex-1 text-left">Users</span>
 						<ChevronDown className="h-4" />
 					</CollapsibleTrigger>
-					<CollapsibleContent className="flex flex-col space-y-4 text-xs">
-						<Element className="font-bold">All Users</Element>
-						<Element>Statistics</Element>
-						<Element>Transactions</Element>
-						<Element>Survey</Element>
+
+					<CollapsibleContent className="relative flex flex-col space-y-4 text-xs">
+						<div className="absolute left-2 top-0 h-11/12 w-0.5 bg-gray-300"></div>
+
+						<div className="relative flex items-center">
+							<div className="absolute left-2 w-8 h-0.5 bg-gray-300"></div>
+							<Element className="font-bold pl-6">All Users</Element>
+						</div>
+
+						<div className="relative flex items-center">
+							<div className="absolute left-2 w-8 h-0.5 bg-gray-300"></div>
+							<Element className="pl-6">Statistics</Element>
+						</div>
+
+						<div className="relative flex items-center">
+							<div className="absolute left-2 w-8 h-0.5 bg-gray-300"></div>
+							<Element className="pl-6">Transactions</Element>
+						</div>
+
+						<div className="relative flex items-center">
+							<div className="absolute left-2 w-8 h-0.5 bg-gray-300"></div>
+							<Element className="pl-6">Survey</Element>
+						</div>
 					</CollapsibleContent>
 				</Collapsible>
 			</SidebarContent>
